87cd8c0626d4acbf6c597a3972ff67b774c60831
ozen-hub <info.ozenhub@gmail.com>
2022-12-31 20:39:04 +0530

    3

--- screens/LoginPage.js
+++ screens/LoginPage.js
@@ -1,12 +1,21 @@
 import {Text, View, StyleSheet} from "react-native";
+import {LinearGradient} from "expo-linear-gradient";
 
 export default function Login() {
     return (
         <View style={styles.container}>
             <View style={styles.top}>
-                <View style={styles.v2}></View>
+                <View style={styles.v2}>
+                    <LinearGradient
+                    colors={['#e67e22','#d35400']}
+                    style={styles.bg}
+                    ></LinearGradient>
+                </View>
                 <View style={styles.v1}>
-
+                    <LinearGradient
+                        colors={['#f39c12','#f1c40f']}
+                        style={styles.bg}
+                    ></LinearGradient>
                 </View>
             </View>
             <View style={styles.middle}></View>
@@ -22,8 +31,6 @@ const styles = StyleSheet.create({
     },
     top: {
         height: 150,
-        borderWidth: 1,
-        borderColor: 'red'
     },
     middle: {
         flex: 1,
@@ -38,17 +45,31 @@ const styles = StyleSheet.create({
     v1: {
         width: 150,
         height: 100,
-        borderColor: 'red',
-        borderWidth: 1,
         transform:[{rotate:'-35deg'}],
-        borderRadius:5
+        borderRadius:5,
+        position:'absolute',
+        right:-10,
+        top:-50
     },
     v2: {
         width: 200,
         height: 150,
-        borderColor: 'red',
-        borderWidth: 1,
         transform:[{rotate:'-35deg'}],
-        borderRadius:5
+        borderRadius:5,
+        position:'absolute',
+        right:-20,
+        top:-60
+    },
+    bg:{
+        flex:1,
+        borderRadius:5,
+        shadowColor: "#000",
+        shadowOffset:{
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
     }
 })
