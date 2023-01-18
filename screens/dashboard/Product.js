daf42431eaab96252c700437cb8f46c19ee2d383
ozen-hub <info.ozenhub@gmail.com>
2023-01-18 21:44:01 +0530

    16

--- screens/dashboard/Product.js
+++ screens/dashboard/Product.js
@@ -1,11 +1,23 @@
 import {useState} from "react";
-import {View, Button, Image, ScrollView, ImageBackground, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {View, Button,TextInput, Image, ScrollView, ImageBackground, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 
 export default function Product() {
     return (
         <View style={styles.container}>
-            <Text>Product</Text>
+            <TextInput style={styles.input}
+                       placeholder={'Display Name'}/>
+            <TextInput style={styles.input}
+                       placeholder='Description'/>
+            <TextInput style={styles.input}
+                       placeholder='Unit Price'/>
+            <TextInput style={styles.input}
+                       placeholder='Qty'/>
+            <TextInput style={styles.input}
+                       placeholder='Selling Price'/>
+            <TouchableOpacity style={styles.btnOuter}>
+                <Text style={styles.btnText}>Submit data</Text>
+            </TouchableOpacity>
         </View>
     )
 }
@@ -13,7 +25,25 @@ export default function Product() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems:'center',
+        alignItems:'stretch',
         justifyContent:'center'
+    },
+    input:{
+        height:35,
+        borderWidth:1,
+        borderColor:'gray',
+        borderRadius:3,
+        marginBottom:10,
+        paddingLeft:5
+    },
+    btnOuter:{
+        height:50,
+        backgroundColor:'blue',
+        alignItems:'center',
+        justifyContent:'center',
+        borderRadius:3
+    },
+    btnText:{
+        color:'white'
     }
 })
